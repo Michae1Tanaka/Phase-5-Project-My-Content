35162ed10ecea858f83a8b3578d3079a0db11c19
Michael Tanaka <tanakam4214@gmail.com>
2023-10-01 11:29:47 -0700

    adds new seed data for content; updated validation bug for User.username; updates db to show content seed data;

--- server/models.py
+++ server/models.py
@@ -23,6 +23,10 @@ class User(db.Model, SerializerMixin):
 
     @password_hash.setter
     def password_hash(self, password):
+        if not password:
+            raise ValueError("Users must enter a password.")
+        if len(password) < 8:
+            raise ValueError("Passwords must be at least 8 characters.")
         password_hash = bcrypt.generate_password_hash(password.encode("utf-8"))
         self._password_hash = password_hash.decode("utf-8")
 
@@ -36,13 +40,7 @@ class User(db.Model, SerializerMixin):
         user_check = User.query.filter_by(username=username).first()
         if user_check:
             raise ValueError(f"This username is already in use.")
-
-    @validates("password")
-    def validates_password(self, key, password):
-        if not password:
-            raise ValueError("Users must enter a password.")
-        if len(password) < 8:
-            raise ValueError("Passwords must be at least 8 characters.")
+        return username
 
     def __repr__(self):
         return f"Username: {self.username}"
@@ -54,6 +52,7 @@ class Content(db.Model, SerializerMixin):
     serialize_rules = "-user_id"
 
     id = db.Column(db.Integer, primary_key=True)
+    creator = db.Column(db.String, nullable=False, server_default="Unknown")
     title = db.Column(db.String, nullable=False)
     thumbnail = db.Column(db.String, nullable=False)
     description = db.Column(db.String, nullable=False)
--- server/seed.py
+++ server/seed.py
@@ -8,12 +8,13 @@ from faker import Faker
 
 # Local imports
 from app import app
-from models import db, User
+from models import db, User, Content
 
 if __name__ == "__main__":
     with app.app_context():
         print("Deleting all records...")
         User.query.delete()
+        Content.query.delete()
 
         fake = Faker()
 
@@ -22,23 +23,72 @@ if __name__ == "__main__":
         users = []
         usernames = []
 
-        for _ in range(30):
-            username = fake.first_name()
-            while username in usernames:
-                username = fake.first_name()
+        for _ in range(10):
+            username = fake.first_name() + "user"
+            while not username or username in usernames:
+                username = fake.first_name() + "user"
+
             usernames.append(username)
 
             user = User(username=username)
-
-            user.password_hash = user.username + "password"
+            user.password_hash = username + "password"
 
             users.append(user)
-        test_user = User(username="potato", is_admin=True)
-        test_user.password_hash = "potato"
+        test_user = User(username="potatoes")
+        test_user.password_hash = "potatoes"
         users.append(test_user)
 
         db.session.add_all(users)
 
         db.session.commit()
+        print("Users Completed")
 
-        print("Complete.")
+        print("Creating Content...")
+
+        video_links = [
+            "https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley",
+            "https://www.youtube.com/watch?v=rfscVS0vtbw&ab_channel=freeCodeCamp.org",
+            "https://www.youtube.com/watch?v=u6gSSpfsoOQ&ab_channel=freeCodeCamp.org",
+            "https://www.youtube.com/watch?v=Z1RJmh_OqeA&ab_channel=freeCodeCamp.org",
+            "https://www.youtube.com/watch?v=0sOvCWFmrtA&ab_channel=freeCodeCamp.org",
+            "https://www.youtube.com/watch?v=yBDHkveJUf4&ab_channel=freeCodeCamp.org",
+            "https://www.youtube.com/watch?v=Z1RJmh_OqeA&t=3s&ab_channel=freeCodeCamp.org",
+            "https://www.youtube.com/watch?v=byHcYRpMgI4&ab_channel=freeCodeCamp.org",
+            "https://www.youtube.com/watch?v=HXV3zeQKqGY&ab_channel=freeCodeCamp.org",
+        ]
+
+        articles_links = [
+            "https://realpython.com/preview/python-tuple/#getting-started-with-pythons-tuple-data-type",
+            "https://realpython.com/preview/python312-new-features/",
+            "https://realpython.com/python312-typing/",
+        ]
+
+        user_ids = [user.id for user in User.query.all()]
+
+        content_data = []
+
+        for x in range(100):
+            content = Content(
+                title=fake.sentence(),
+                creator=fake.name(),
+                thumbnail=fake.image_url(),
+                description=fake.sentence(),
+                uploaded_at=fake.date_time_between(start_date="-1y", end_date="now"),
+                created_at=fake.date_time_between(start_date="-2y", end_date="-1y"),
+                user_id=rc(user_ids),
+            )
+            if x < 33:
+                content.type = "Videos"
+                content.url = rc(video_links)
+            elif 33 <= x < 66:
+                content.type = "Articles"
+                content.url = rc(articles_links)
+            else:
+                content.type = "Other"
+                content.url = fake.url()
+
+            content_data.append(content)
+
+        db.session.add_all(content_data)
+        db.session.commit()
+        print("Content Created!")
